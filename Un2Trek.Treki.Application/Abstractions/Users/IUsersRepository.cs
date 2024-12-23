1238fdbbef99fbb6b57b07eb00bce0f6a642310f
victorvalenciamunoz <66370480+victorvalenciamunoz@users.noreply.github.com>
2024-12-23 15:24:57 +0100

    Tests (#3)
    * Add cancellation token
    
    * testing pr flows
    
    * Add tests
    
    * Fix yml file
    
    ---------
    
    Co-authored-by: Victor Valencia <vvalencia@biosmartdata.eu>

---
+++ Un2Trek.Treki.Application/Abstractions/Users/IUsersRepository.cs
@@ -0,0 +1,8 @@
+﻿using Un2Trek.Trekis.Domain;
+
+namespace Un2Trek.Trekis.Application.Abstractions.Users;
+
+public interface IUsersRepository
+{
+    Task<ApplicationUser?> GetUserWithCapturesAsync(string userId, CancellationToken cancellationToken);
+}
