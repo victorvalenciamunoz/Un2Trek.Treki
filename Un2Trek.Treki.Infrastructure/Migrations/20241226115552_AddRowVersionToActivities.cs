649e9352050f1c0365fd2a1e40387fc02ec34302
victorvalenciamunoz <66370480+victorvalenciamunoz@users.noreply.github.com>
2024-12-26 13:02:59 +0100

    Add migration (#7)
    * change to windows-latest
    
    * Clean extra steps
    
    * Add EF migration to CI and add one migration to test
    
    * Fix yml file
    
    ---------
    
    Co-authored-by: Victor Valencia <vvalencia@biosmartdata.eu>

---
+++ Un2Trek.Treki.Infrastructure/Migrations/20241226115552_AddRowVersionToActivities.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Un2Trek.Trekis.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRowVersionToActivities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "RowVersion",
+                table: "ActivityTrekis",
+                type: "rowversion",
+                rowVersion: true,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RowVersion",
+                table: "ActivityTrekis");
+        }
+    }
+}
