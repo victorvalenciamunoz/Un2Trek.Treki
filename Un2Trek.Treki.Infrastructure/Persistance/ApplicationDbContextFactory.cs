ea4c613593f9011739e4c4d0bca6e800401c1ea3
Victor Valencia <vvalencia@biosmartdata.eu>
2024-12-27 11:52:15 +0100

    Add ApplicationDbContextFactory

---
+++ Un2Trek.Treki.Infrastructure/Persistance/ApplicationDbContextFactory.cs
@@ -0,0 +1,17 @@
+﻿namespace Un2Trek.Trekis.Infrastructure.Persistance;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+{
+    public ApplicationDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+                
+        var connectionString = Environment.GetEnvironmentVariable("ConnectionStrings__DefaultConnection");
+        optionsBuilder.UseSqlServer(connectionString);
+
+        return new ApplicationDbContext(optionsBuilder.Options);
+    }
+}
