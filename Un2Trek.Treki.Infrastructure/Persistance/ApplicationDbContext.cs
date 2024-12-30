83e0466637d60fe814d776c36717e604ca4a26a7
victorvalenciamunoz <66370480+victorvalenciamunoz@users.noreply.github.com>
2024-12-30 10:45:06 +0100

    use dbcontext (#17)
    Co-authored-by: Victor Valencia <vvalencia@biosmartdata.eu>

--- Un2Trek.Treki.Infrastructure/Persistance/ApplicationDbContext.cs
+++ Un2Trek.Treki.Infrastructure/Persistance/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Un2Trek.Trekis.Domain;
 
@@ -7,6 +8,9 @@ namespace Un2Trek.Trekis.Infrastructure;
 
 public sealed class ApplicationDbContext : IdentityDbContext<ApplicationUser>
 {
+    public ApplicationDbContext()
+    {}
+
     public ApplicationDbContext(DbContextOptions options) : base(options) { }
 
     public DbSet<Treki> Trekis => Set<Treki>();
@@ -27,5 +31,31 @@ public sealed class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
     {
         return base.SaveChangesAsync(cancellationToken);
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {            
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: true)
+                .AddEnvironmentVariables()
+                .AddCommandLine(Environment.GetCommandLineArgs())
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new InvalidOperationException("Connection string 'DefaultConnection' is not defined.");
+            }
+
+            optionsBuilder.UseSqlServer(connectionString);
+         
+            var loggerFactory = LoggerFactory.Create(builder => builder.AddConsole());
+            optionsBuilder.UseLoggerFactory(loggerFactory);
+        }
+
+        base.OnConfiguring(optionsBuilder);
     }
 }
