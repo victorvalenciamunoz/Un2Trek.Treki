57c437f118811d45aa04878cd2937453f81e7b5b
Victor Valencia <vvalencia@biosmartdata.eu>
2024-12-31 08:21:16 +0100

    Modify GetTrekisAround proc creation migration

--- Un2Trek.Treki.Infrastructure/Migrations/20241105144837_AddGetTrekisAroundStoredProcedure.cs
+++ Un2Trek.Treki.Infrastructure/Migrations/20241105144837_AddGetTrekisAroundStoredProcedure.cs
@@ -7,28 +7,29 @@ namespace Un2Trek.Trekis.Infrastructure.Migrations;
 public partial class AddGetTrekisAroundStoredProcedure : Migration
 {
     protected override void Up(MigrationBuilder migrationBuilder)
-    {
+    {        
         migrationBuilder.Sql(@"
-               IF OBJECT_ID('dbo.GetTrekisAround', 'P') IS NOT NULL
-                   DROP PROCEDURE dbo.GetTrekisAround;
-           ");
-
-        // Create the procedure without wrapping in a transaction
+            IF OBJECT_ID('dbo.GetTrekisAround', 'P') IS NOT NULL
+                DROP PROCEDURE dbo.GetTrekisAround;
+        ");
+     
         migrationBuilder.Sql(@"
-               CREATE PROCEDURE [dbo].[GetTrekisAround]
-                   @currentLatitude FLOAT,
-                   @currentLongitude FLOAT,
-                   @threshold INT
-               AS
-               BEGIN
-                   DECLARE @source GEOGRAPHY = GEOGRAPHY::Point(@currentLatitude, @currentLongitude, 4326);
+            EXEC('
+                CREATE PROCEDURE [dbo].[GetTrekisAround]
+                    @currentLatitude FLOAT,
+                    @currentLongitude FLOAT,
+                    @threshold INT
+                AS
+                BEGIN
+                    DECLARE @source GEOGRAPHY = GEOGRAPHY::Point(@currentLatitude, @currentLongitude, 4326);
 
-                   SELECT trekis.*
-                   FROM trekis
-                   WHERE @source.STDistance(GEOGRAPHY::Point(Latitude, Longitude, 4326)) <= @threshold
-                     AND trekis.IsActive = 1;
-               END;
-           ", suppressTransaction: true);
+                    SELECT trekis.*
+                    FROM trekis
+                    WHERE @source.STDistance(GEOGRAPHY::Point(Latitude, Longitude, 4326)) <= @threshold
+                      AND trekis.IsActive = 1;
+                END
+            ');
+        ", suppressTransaction: true);
     }
 
     protected override void Down(MigrationBuilder migrationBuilder)
